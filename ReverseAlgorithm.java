6ef5bee17e9faf3f2dd426dae329c7f22f4489de
Shraddha Karle <97306341+Shraddha-karle@users.noreply.github.com>
2022-01-12 12:40:58 +0530

    Add files via upload

---
+++ ReverseAlgorithm.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class ReverseAlgorithm {
+    public static void main(String[] args) {
+        int[]arr=new int[]{1,2,3,4,5,6,7,8,9,10};
+        //Printing array
+        System.out.println("Original Array is : ");
+        for (int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+        //Printing reverse array
+        System.out.println("Reversed array is : ");
+        for (int i= arr.length-1;i>=0;i--){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
