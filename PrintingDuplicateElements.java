fefb1f6edafb0a6428e5cb827a0da1582b654f9d
Shraddha Karle <97306341+Shraddha-karle@users.noreply.github.com>
2022-02-14 10:47:07 +0530

    Add files via upload

---
+++ PrintingDuplicateElements.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class PrintingDuplicateElements {
+    public static void main(String[] args) {
+        int arr[]={2,3,4,4,2,5,6,6,7,8,9,9};
+        System.out.println("Duplicate elements of array are" );
+        for (int i=0;i< arr.length;i++){
+            for (int j=i+1;j< arr.length;j++){
+                if(arr[i]==arr[j])
+                    System.out.println(arr[j]);
+            }
+        }
+
+    }
+    }
+
